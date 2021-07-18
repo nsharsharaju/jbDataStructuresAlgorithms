eb4160b16ae02f84cabbabf1dd3c8164cd511034
Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>
2021-07-18 08:32:40 +0530

    Add otpRLE answer Signed-off-by: Sri Harsha Nadimpalli <harsha.sri@investwellonline.com>

---
+++ l8_wZpuK6xL1Lw/optRLE.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    string opt;
+    opt += s[0];
+    int cnt = 0;
+    string tempCnt = "";
+    for (int i = 1; i < s.length(); i++)
+    {
+        if ('0' <= s[i] <= '9')
+        {
+            tempCnt += s[i];
+        }
+        else
+        {
+            cnt += stoi(tempCnt);
+            if (opt[opt.length() - 1] != s[i])
+            {
+                opt += to_string(cnt);
+                opt += s[i];
+                cnt = 0;
+            }
+        }
+        tempCnt = "";
+    }
+    cnt += stoi(tempCnt);
+    opt += to_string(cnt);
+    cout << opt << endl;
+    return;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
